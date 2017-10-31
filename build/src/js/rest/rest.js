8fb4c7263246c9e174b2646c149e3455083b689b
Zathrian <raaghavbhardwaj@gmail.com>
2017-10-31 18:38:32 -0400

    added access control headers for MS Edge

--- build/src/js/rest/rest.js
+++ build/src/js/rest/rest.js
@@ -5,16 +5,16 @@ const server_address = "https://dermadetect.appspot.com/api/"  //"http://localho
 
 export function get(args, page, limit)
 {
-    
+    /*
     return fetch(server_address + args + "?_page="+page + "&_limit="+limit+"&_sort=id&_order=desc")
     .then(response => response.json())
     .then(data => { console.log(data);  return data } );
-
-    /*
+*/
+    
     return axios.get( server_address + args + "?_page="+page + "&_limit="+limit+"&_sort=id&_order=desc").then( (data) => {
         return data.data;
     } )
-    */
+    
 }
 
 export function post(target, args)
